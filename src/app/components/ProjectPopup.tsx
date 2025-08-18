ba1e6ab6dc6bf52847b3cd2e8333ef681ff35368
Jess <jlbachel@umich.edu>
2025-08-18 10:14:12 -0400

    adding in styling for project popups

--- src/app/components/ProjectPopup.tsx
+++ src/app/components/ProjectPopup.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { ProjectCardsType } from "../types/Projects";
+import Button from "./Button";
 
 interface Props extends ProjectCardsType {
     onClose: () => void
@@ -13,19 +14,68 @@ const ProjectPopup:React.FC<Props> = (props: Props) => {
         onClose()
     }
 
+    const openLink = (link: string) => {
+        window.open(link, "_blank");
+    }
+
     return (
         <div className="popup-window">
-            <button onClick={() => closeWindow()}>x</button>
+            <div id="project-banner">
+                <h4>{title}</h4>
+                <button onClick={() => closeWindow()}>x</button>
+            </div>
+            <div id="project-img">
+                <img src={`${image}`} width="60%" height="60%"/>
+            </div>
+            <div id="tech-stack">
+                {techStack?.map((key, index)=> (
+                    <Button text={key}  onClick={()=>console.log()}/>
+                ))}
+            </div>
+            <div id="description">
+                {description}
+            </div>
+            <div id="buttons">
+                <Button text="codebase" onClick={()=>openLink(repolink)}/>
+                <Button text="website" onClick={()=>openLink(link)}/>
+            </div>
 
 
         <style jsx>
             {`
             div.popup-window {
-                position: absolute;
-                height: 500px;
-                width: 200px;
+                position: fixed;
+                // height: 500px;
+                width: 400px;
                 background-color: peachpuff;
                 border: 1px solid black;
+            }
+            #project-img {
+                text-align: center;
+            }
+            #project-banner {
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+                margin: 10px;
+            }
+            #buttons {
+                display: flex;
+                justify-content: space-around;
+            }
+            #tech-stack {
+                display: flex;
+                justify-content: space-around;
+            }
+            #description {
+                margin: 5px 10px;
+                border: 1px solid black;
+                border-radius: 5px;
+                padding: 5px;
+                background-color: cornsilk;
+            }
+            button:hover {
+                cursor: pointer;
             }
 
             
